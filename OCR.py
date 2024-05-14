8804caa67c8f2aa5ab9531dd1c542efa7132a333
Pierre Collado <rpierrecollado@gmail.com>
2024-05-15 00:04:24 +0800

    sfgd

--- OCR.py
+++ OCR.py
@@ -5,4 +5,11 @@ img=cv2.imread('Capture.JPG')
 reader = easyocr.Reader(['en'],gpu=False)
 result=reader.readtext(img)
 
-print(result)
+chest='CN09Z03AK1'
+
+print(result[0][1])
+
+if chest==result[0][1]:
+    print('SAME!!!')
+else:
+    print('not same')
