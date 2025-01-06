fc6b0a0b3af933c40b5b6ae7eb0031689499147d
violencepierre <71010678+violencepierre@users.noreply.github.com>
2025-01-06 10:34:42 +0800

    ssss

--- mainframe.py
+++ mainframe.py
@@ -3,12 +3,17 @@ from MappingNonLFF import mappingnonlffaktion
 from MappingLFFRobot import mainloop
 from test import testing
 from os import system
+from time import sleep
 
 
 x=1
 
 while x != 0:
     ans=int(input("Choose what u wanna do: \n1. create hierarchy, \n2. create nonLFF mapping template, or \n3. use the LFF mapping rob0t, or \n4. exit cuz im a dummy \n\n(ENTER AN INTEGER FOR THE LOVE OF GOD) "))
+    print("you entered ", ans, " lmao wtf")
+
+    sleep(2)
+
     if ans==4:
         print("----------------------------")
         print("----------------------------")
@@ -18,19 +23,52 @@ while x != 0:
         x=0
 
     elif ans==1:
-        hiymaker()        
+        print("running hierarchy maker")      
+        hiymaker() 
+        sleep(2) 
+        system("cls")
+        print("----------------------------")
+        print("COMPLETED")
+        print("----------------------------")
+        print("----------------------------")
+
 
     elif ans==2:
+        print("running nonlff template creator")   
         mappingnonlffaktion()
+        sleep(2)
+        system("cls")
+        print("----------------------------")
+        print("COMPLETED")
+        print("----------------------------")
+        print("----------------------------")
 
     elif ans==3:
+        print("running lff r0bot") 
         mainloop()
-
+        sleep(2)
+        system("cls")
+        print("----------------------------")
+        print("----------------------------")
+        print("----------------------------")
+        print("----------------------------")
+        print("COMPLETED")
+        print("----------------------------")
+        print("----------------------------")
+        print("----------------------------")
+   
     else:
-
-        print("bro what the f*ck i gave u a choice already")
+        system("cls")
+        sleep(2)
         print("----------------------------")
         print("----------------------------")
         print("----------------------------")
         print("----------------------------")
+        print("bro what the f*ck i gave u a bunch of choices already")
+        print("----------------------------")
+        print("----------------------------")
+        print("----------------------------")
+        print("----------------------------")
+        sleep(2)
+        system("cls")
 
