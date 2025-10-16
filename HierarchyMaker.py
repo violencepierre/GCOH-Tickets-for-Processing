cffe1f314650d0e02e1a3bdc594d876bee2e6a4d
violencepierre <rpierrecollado@gmail.com>
2025-10-17 04:25:19 +0800

    allllll

--- HierarchyMaker.py
+++ HierarchyMaker.py
@@ -1,22 +1,23 @@
 import pandas as pd
 import networkx as satan
 
+
 def hiymaker():
 
     print("running")
     df = pd.read_excel('pandas.xlsx')
 
-
-    G = satan.from_pandas_edgelist(df, 'Parent', 'Node', create_using=satan.DiGraph)
+    G = satan.from_pandas_edgelist(
+        df, 'Parent', 'Node', create_using=satan.DiGraph)
     roots = [v for v, d in G.in_degree() if d == 0]
     leaves = [v for v, d in G.out_degree() if d == 0]
-    num=0
+    num = 0
     all_paths = []
     for root in roots:
         for leaf in leaves:
             paths = satan.all_simple_paths(G, root, leaf)
             all_paths.extend(paths)
-            num=num+1
+            num = num+1
             print(num)
 
     for node in satan.nodes_with_selfloops(G):
@@ -24,8 +25,7 @@ def hiymaker():
 
     output = pd.DataFrame(sorted(all_paths)).add_prefix('level').fillna(' ')
 
-    output.to_excel('pandas.xlsx',sheet_name='Output')
+    output.to_excel('pandas.xlsx', sheet_name='Output')
 
-    #use 0a[Blank] this will solve ur problems
+    # use 0a[Blank] this will solve ur problems
     print("its joeber")
-
